a747965a4bbfa7b555c2fc029c0f3c3fd8a811f8
rajankumarpatel <rajankumarpatel@rajankumars-mbp.home>
2018-04-24 18:52:50 -0400

    Updated java file

--- src/Utility/EOMU_Readjsonfile.java
+++ src/Utility/EOMU_Readjsonfile.java
@@ -3,44 +3,44 @@ package Utility;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
-
-import org.json.JSONException;
 import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
 
 public class EOMU_Readjsonfile {
 
-	public static void main(String[] args) throws org.json.simple.parser.ParseException, JSONException, FileNotFoundException, IOException {
-		
-		
+	public static void main(String[] args) throws org.json.simple.parser.ParseException {
+
 		JSONParser parser = new JSONParser();
-		JSONArray a = (JSONArray) parser.parse(new FileReader(EOMU_constants.JSON_FILEPATH));
 
-		  for (Object o : a)
-		  {
-		    JSONObject person = (JSONObject) o;
+		try {
+			JSONArray a = (JSONArray) parser.parse(new FileReader(EOMU_constants.JSON_FILEPATH));
 
-		    String name = (String) person.get("name");
-		    System.out.println(name);
+			for (Object o : a) {
+				JSONObject person = (JSONObject) o;
 
-		    String city = (String) person.get("city");
-		    System.out.println(city);
+				String name = (String) person.get("name");
+				System.out.println(name);
 
-		    String job = (String) person.get("job");
-		    System.out.println(job);
+				String city = (String) person.get("city");
+				System.out.println(city);
 
-		    JSONArray cars = (JSONArray) person.get("cars");
+				String job = (String) person.get("job");
+				System.out.println(job);
 
-		    for (Object c : cars)
-		    {
-		      System.out.println(c+"");
-		    }
-    
-        }
-    
-	  }
-		
-   }
+				JSONArray cars = (JSONArray) person.get("cars");
 
+				for (Object c : cars) {
+					System.out.println(c + "");
+				}
+			}
+		} catch (FileNotFoundException e) {
+			System.out.println("No such file found");
 
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
+	}
+
+}
